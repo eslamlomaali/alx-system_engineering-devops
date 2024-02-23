fef5f7bba22cabf85d9881b4b051248d94f4ebdc
eslamlomaali <eslam@gmail.com>
2024-02-24 01:54:02 +0200

    done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Creates a file
 file { '/tmp/school':
-  mode    => '0744',
-  content => 'I love Puppet',
-  path    => '/tmp/school',
   ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data'
+  group   => 'www-data',
+  content => 'I love Puppet'
 }

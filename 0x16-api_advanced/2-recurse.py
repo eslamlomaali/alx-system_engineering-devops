e2f3511f2b531e6fd116f12abfa1e9ea924210cc
eslamlomaali <eslam@gmail.com>
2024-04-09 12:04:07 +0000

    done

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,12 +4,12 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-        """Returns a list of titles of all post for subreddit."""
+    """list of titles of all posts for subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "0x16-api_advanced:project:\
 v1.0.0 (by /u/firdaus_cartoon_jr)"
-     }
+    }
     params = {
         "after": after,
         "count": count,
@@ -27,5 +27,5 @@ v1.0.0 (by /u/firdaus_cartoon_jr)"
         hot_list.append(c.get("data").get("title"))
 
     if after is not None:
-	return recurse(subreddit, hot_list, after, count)
+        return recurse(subreddit, hot_list, after, count)
     return hot_list
